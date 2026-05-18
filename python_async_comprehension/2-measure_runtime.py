75debb3c9bbe0d31ec14d539d09aa4d911b4d6be
jessewalu <j.walusansa@alustudent.com>
2026-05-18 08:58:10 +0200

    done

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -18,9 +18,9 @@ async def measure_runtime() -> float:
     """
     start_time = time.time()
 
-    await asyncio.gather(
-        *[async_comprehension() for _ in range(4)]
-    )
+    await asyncio.gather(*[
+        async_comprehension() for _ in range(4)
+    ])
 
     end_time = time.time()
 
