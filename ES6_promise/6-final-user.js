6de827a2ca770219a6bea9c7fc9173c2fd41f3aa
jessewalu <j.walusansa@alustudent.com>
2026-07-01 00:33:10 +0200

    updated the files

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,11 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+  return Promise.all([
+    signUpUser(firstName, lastName)
+      .then((value) => ({ status: 'fulfilled', value }))
+      .catch((value) => ({ status: 'rejected', value })),
+    uploadPhoto(fileName)
+      .then((value) => ({ status: 'fulfilled', value }))
+      .catch((value) => ({ status: 'rejected', value })),
+  ]);
 }
