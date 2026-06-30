a47cb4fbd60029b547952d53621cd7131fe2b43f
jessewalu <j.walusansa@alustudent.com>
2026-06-30 18:36:25 +0200

    updated the files

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,16 +7,16 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    total_logs = nginx_collection.count_documents({})
+    total_logs = nginx_collection.count()
     print("{} logs".format(total_logs))
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
+        count = nginx_collection.find({"method": method}).count()
         print("\tmethod {}: {}".format(method, count))
 
-    status_check = nginx_collection.count_documents(
+    status_check = nginx_collection.find(
         {"method": "GET", "path": "/status"}
-    )
+    ).count()
     print("{} status check".format(status_check))
