5226a6a16bee6b45d370f0862b48fba5ba824751
jessewalu <j.walusansa@alustudent.com>
2026-07-05 16:47:28 +0200

    updated the files

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
