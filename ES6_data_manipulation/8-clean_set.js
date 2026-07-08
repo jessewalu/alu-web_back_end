8c7320398fd42f50b347190d8f3bc48b71561dbf
jessewalu <j.walusansa@alustudent.com>
2026-07-08 13:15:35 +0200

    updated the files

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,13 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
 
   const result = [];
   set.forEach((value) => {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
+    const strValue = String(value);
+    if (strValue.startsWith(startString)) {
+      result.push(strValue.slice(startString.length));
     }
   });
 
