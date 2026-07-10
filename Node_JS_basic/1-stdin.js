8d2440325525368d7c1f7f8b989a49ae72028495
jessewalu <j.walusansa@alustudent.com>
2026-07-10 08:04:34 +0200

    updated all the files

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,28 @@
-const readline = require('readline');
-
 console.log('Welcome to Holberton School, what is your name?');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  terminal: false,
+process.stdin.setEncoding('utf8');
+
+let buffer = '';
+
+function flushLine(line) {
+  console.log(`Your name is: ${line.replace(/\r$/, '')}`);
+}
+
+process.stdin.on('data', (chunk) => {
+  buffer += chunk;
+
+  let newlineIndex = buffer.indexOf('\n');
+  while (newlineIndex !== -1) {
+    flushLine(buffer.slice(0, newlineIndex));
+    buffer = buffer.slice(newlineIndex + 1);
+    newlineIndex = buffer.indexOf('\n');
+  }
 });
 
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input.trim()}`);
-});
-
-rl.on('close', () => {
+process.stdin.on('end', () => {
+  if (buffer.length > 0) {
+    flushLine(buffer);
+    buffer = '';
+  }
   console.log('This important software is now closing');
 });
