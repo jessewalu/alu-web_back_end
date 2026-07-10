e80aee3f720dd2f29e81d47badf95d4f7bee9efa
jessewalu <j.walusansa@alustudent.com>
2026-07-10 07:44:15 +0200

    created all the files

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
